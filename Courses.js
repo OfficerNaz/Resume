86b3b965986bf6685fd4d9213275e3c93320ea92
OfficerNaz <97906659+OfficerNaz@users.noreply.github.com>
2022-09-08 21:52:09 -0400

    Initial commit

---
+++ Courses.js
@@ -0,0 +1,11 @@
+function openPage() {
+    var x = document.getElementById("search").value;
+    
+    if(x === "w"){
+        window.open("/Communication.html");
+    }
+
+    if(x === "o"){
+    window.open("/OOP.html")
+
+}}
